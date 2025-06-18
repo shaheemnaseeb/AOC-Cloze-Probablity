f1b16d6928e5e213f4c3d139e4d1475121b483bc
Shaheem Khan <62075510+shaheemnaseeb@users.noreply.github.com>
2025-06-18 13:49:53 +0200

    Update AOC1_v2.java

--- v2/AOC1_v2.java
+++ v2/AOC1_v2.java
@@ -5,14 +5,12 @@ public class AOC1_v2 {
         int level = 2;
         int baseCharge = 10;
         int interim = level * 3;
-        int R = baseCharge + interim - 5;
-
-        R += _____;
 
         int tax = (level > 0) ? 2 * level : 0;
-        R = R + tax;
+        int interest = _______;
+        interest += tax;
 
-        if (R == 18 && level == 3) {
+        if (interest == 6 && level == 3) {
             System.out.println("✔ Correct atom reconstructed!");
         } else {
             System.out.println("✘ Wrong or missing pre‐increment atom (R=" + R +
